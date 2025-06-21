04043ee90698168dba12af672c85eac79586aa24
0qtist <77591116+oqtist@users.noreply.github.com>
2025-06-21 12:00:01 -0300

    [Backend] • Implementada a rota para a função determinada no commit anterior. (Ops.)

--- Backend/routes/veiculos.js
+++ Backend/routes/veiculos.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const routerVeiculos = express.Router()
-import { cadastroVeiculo, atualizarVeiculo, destruirVeiculo, listarVeiculos } from '../controllers/veiculos.js'
+import { cadastroVeiculo, atualizarVeiculo, destruirVeiculo, listarVeiculos, veiculosExclusaoConta } from '../controllers/veiculos.js'
 import { verificarToken } from '../middleware/auth.js'
 
 routerVeiculos.post('/cadastro-veiculos', verificarToken, cadastroVeiculo)
 routerVeiculos.put('/veiculos/:id', verificarToken, atualizarVeiculo)
 routerVeiculos.delete('/veiculos/:id', verificarToken, destruirVeiculo)
 routerVeiculos.get('/veiculos', verificarToken, listarVeiculos)
+routerVeiculos.delete('/veiculos-exclusao-conta', verificarToken, veiculosExclusaoConta)
 
 export { routerVeiculos }
