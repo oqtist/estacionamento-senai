cdb9e154edeca3010e1957959fb7d36324d3fe18
0qtist <77591116+oqtist@users.noreply.github.com>
2025-06-23 11:55:25 -0300

    [Backend] Hotfix #1

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -2,12 +2,11 @@ import express from 'express'
 const routerAdmin = express.Router()
 
 import { verificarAdmin } from '../middleware/auth_admin.js'
-import { listarAcessos, listarUsuarios, listarVeiculos, listarVagasOcupadas, infoGetter } from '../controllers/admin.js'
+import { listarAcessos, listarUsuarios, listarVeiculos, listarVagasOcupadas } from '../controllers/admin.js'
 
 routerAdmin.get('/admin/acessos/', verificarAdmin, listarAcessos)
 routerAdmin.get('/admin/usuarios/', verificarAdmin, listarUsuarios)
 routerAdmin.get('/admin/veiculos/', verificarAdmin, listarVeiculos)
 routerAdmin.get('/admin/vagas/', verificarAdmin, listarVagasOcupadas)
-routerAdmin.post('/admin/info/', verificarAdmin, infoGetter) //contraditório por natureza, mas visto que postamos informações na requisição..
 
 export { routerAdmin }
