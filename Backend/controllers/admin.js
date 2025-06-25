920b9fe42be8a504efccba5dfcc610d72d7f7a23
0qtist <77591116+oqtist@users.noreply.github.com>
2025-06-25 15:20:37 -0300

    [Backend] • Teste paginação

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -4,12 +4,16 @@ import { Usuario } from '../models/users.js'
 
 export const listarUsuarios = async (req, res) => {
     try {
-        const listaUsuarios = await Usuario.findAll()
-        if (listaUsuarios.length == 0) {
+        const pagina = Number(req.body.pagina) || 1
+        const limite = Number(req.body.limite) || 10
+        const offset = (pagina - 1) * limite
+
+        const { count, rows } = await Usuario.findAndCountAll({ offset, limite, order: [['nome', DESC]] })
+        if (count == 0) {
             res.status(500).send({ mensagem: 'Nenhum usuário encontrado.' })
             return
         }
-        res.status(200).send({ listaUsuarios })
+        res.status(200).send({ totalUsuarios: count, totalPaginas: Math.ceil(count / limite), paginaAtual: pagina, listarUsuarios: rows })
     } catch (err) {
         console.log(err)
     }
@@ -17,17 +21,22 @@ export const listarUsuarios = async (req, res) => {
 
 export const listarAcessos = async (req, res) => {
     try {
-        const listaAcessos = await Acessos.findAll({
+        const pagina = Number(req.body.pagina) || 1
+        const limite = Number(req.body.limite) || 10
+        const offset = (pagina - 1) * limite
+
+        const { count, rows } = await Acessos.findAndCountAll({
+            offset, limite, order: [['data_entrada', DESC]],
             include: [
                 { model: Usuario },
                 { model: Veiculos }
             ]
         })
-        if (listaAcessos.length == 0) {
+        if (count == 0) {
             res.status(500).send({ mensagem: 'Nenhum acesso encontrado.' })
             return
         }
-        res.status(200).send({ listaAcessos })
+        res.status(200).send({ totalAcessos: count, totalPaginas: Math.ceil(count / limite), paginaAtual: pagina, listarAcessos: rows })
     } catch (err) {
         console.log(err)
     }
@@ -35,12 +44,16 @@ export const listarAcessos = async (req, res) => {
 
 export const listarVeiculos = async (req, res) => {
     try {
-        const listaVeiculos = await Veiculos.findAll({ include: Usuario })
-        if (listaVeiculos.length == 0) {
+        const pagina = Number(req.body.pagina) || 1
+        const limite = Number(req.body.limite) || 10
+        const offset = (pagina - 1) * limite
+
+        const { count, rows } = await Veiculos.findAll({ offset, limite, order: [['modelo', DESC]], include: Usuario })
+        if (count == 0) {
             res.status(500).send({ mensagem: 'Nenhum veículo encontrado.' })
             return
         }
-        res.status(200).send(listaVeiculos)
+        res.status(200).send({ totalVeiculos: count, totalPaginas: Math.ceil(count / limite), paginaAtual: pagina, listarVeiculos: rows })
     } catch (err) {
         console.log(err)
     }
@@ -48,7 +61,12 @@ export const listarVeiculos = async (req, res) => {
 
 export const listarVagasOcupadas = async (req, res) => {
     try {
+        const pagina = Number(req.body.pagina) || 1
+        const limite = Number(req.body.limite) || 10
+        const offset = (pagina - 1) * limite
+
         const { count, rows } = await Acessos.findAndCountAll({
+            offset, limite, order: [['data_entrada', DESC]],
             where: {
                 data_saida: null
             },
@@ -57,13 +75,16 @@ export const listarVagasOcupadas = async (req, res) => {
                 { model: Veiculos }
             ]
         })
+
         if (count == 0) {
             res.status(400).send({ mensagem: 'Nenhuma vaga ocupada.' })
             return
         }
         res.status(200).send({
-            contagemOcupadas: count,
-            acessos: rows
+            totalVagasOcupadas: count,
+            totalPaginas: Math.ceil(count / limite),
+            paginaAtual: pagina,
+            listarVagasOcupadas: rows
         })
     } catch (err) {
         console.log(err)
